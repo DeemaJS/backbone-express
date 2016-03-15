ba9555e70bcc51d3a1c3242121c34b859b1c5a23
DeemaJS <deeema@outlook.com>
2016-03-15 17:31:25 +0200

    Simple request add

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var CryptoJS = require("crypto-js");
- 
+var request = require('request');
+
 var data = [{id: 1}, {id: 2}]
  
 // Encrypt 
@@ -11,3 +12,10 @@ var bytes  = CryptoJS.AES.decrypt(ciphertext.toString(), 'secret key 123');
 var decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
  
 console.log(decryptedData);
+
+// request
+request('http://www.google.com', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body) // Show the HTML for the Google homepage. 
+  }
+});
